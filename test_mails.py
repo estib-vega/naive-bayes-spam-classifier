5e7471e440747d3d738a95f2955872e0b08b715f
Esteban Vega <stron@me.com>
2018-05-13 13:43:54 +0200

    Functions for probability calculations used in Bayes. Added test mails for debugging
    TODO:
    - Debug
    - Reduce code redundancy through segmentation
    - Implement Naive Bayes for different cases (multiple words, smoothing, long emails)

---
+++ test_mails.py
@@ -0,0 +1,11 @@
+prob71 = []
+
+# mails from course work
+prob71.append(["FREE MONEY", 1])
+prob71.append(["FREE CASINO MONEY", 1])
+prob71.append(["CASINO EVENT", 1])
+prob71.append(["WENT TO MACAU", 0])
+prob71.append(["WON MONEY MACAU CASINO", 0])
+prob71.append(["GREETINGS FROM HOLIDAYS", 0])
+prob71.append(["MAKE PROFIT", 0])
+prob71.append(["MAKE SCRIPT", 0])
