8b548e71d4b8eea31250eabca0bed5af3e2bac7a
Esteban Vega <stron@me.com>
2018-05-13 14:19:26 +0200

    Debugged faulty results while calculating the probability of a word

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from test_mails import prob71 as mails
-from naive_bayes import n_b
+from naive_bayes import n_b, word_class_prob, class_prob, word_prob
 
 if __name__ == "__main__":
     prob_1 = n_b(1, 'free', mails)
@@ -9,6 +9,7 @@ if __name__ == "__main__":
 
     print("Probability of class 1 given 'free':", prob_1)
     print("Probability of class 0 given 'free':", prob_2)
+    print("total:", prob_1 + prob_2)
     print("Probability of class 1 given 'money':", prob_3)
     print("Probability of class 0 given 'money':", prob_4)
-
+    print("total:", prob_3 + prob_4)
--- naive_bayes.py
+++ naive_bayes.py
@@ -8,53 +8,41 @@
 """
 
 # P(A) -> probability of a class in a set of labeled emails
-def class_prob(cl, set):
+def class_prob(cl, _set):
     class_instances = 0
     
     # count how many times the class appears
     # in the set
-    for _, c in set:
+    for _, c in _set:
         if c == cl:
             class_instances += 1
     
     # number of total instances
-    total = len(set)
+    total = len(_set)
 
     return class_instances / total
 
 # P(B) -> probability of a word in a set of labeled emails
-def word_prob(wd, set):
-    word_instances = 0
+def word_prob(wd, _set):
+    # the probability of a word is the sum of 
+    # the probability of a word given a class times the probability of a class
+    # for all clases (1, 0)
 
-    # all emails contain a string of text
-    # with multiple words separated by spaces
-    # add them all together
-    total_text = ""
+    # P(B) -> P(B|1) * P(1) + P(B|0) * P(0) 
+    p_b = word_class_prob(wd, 1, _set) * class_prob(1, _set)
+    p_b += word_class_prob(wd, 0, _set) * class_prob(0, _set)
 
-    for w, _ in set:
-        total_text += w + " "
-    
-    # convert text to list of words
-    word_list = list(total_text.split())
-    word_list_length = len(word_list)
-
-    # count word appearances in word list
-    for w in word_list:
-        word = w.strip().upper()
-        if word == wd:
-            word_instances += 1
-    
-    return word_instances / word_list_length
+    return p_b
 
 # P(B|A) -> probability of a word given a class, inside a set
 # of labeled emails
-def word_class_prob(wd, cl, set):
+def word_class_prob(wd, cl, _set):
     word_instances = 0
 
     # gather all the words for the given class
     total_class_text = ""
 
-    for w, c in set:
+    for w, c in _set:
         if c == cl:
             total_class_text += w + " "
 
@@ -72,6 +60,10 @@ def word_class_prob(wd, cl, set):
 
     # P(A|B) -> Naive Bayes for a single word. Probabilty of a class
     # given a word, in a ste of labeled emails
-def n_b(cl, wd, set):
+def n_b(cl, wd, _set):
     word = wd.strip().upper()
-    return word_class_prob(word, cl, set) * class_prob(cl, set) / word_prob(word, set)
+    p_ba = word_class_prob(word, cl, _set)
+    p_a = class_prob(cl, _set)
+    p_b = word_prob(word, _set)
+
+    return p_ba * p_a / p_b
